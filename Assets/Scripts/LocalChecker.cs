8eb773438e9e18f1fc816268d9f2c248f465c6ee
Mickaël BERTHELOT <41340047+crread@users.noreply.github.com>
2023-04-12 12:20:25 +0200

    [NETWORK] update prefabs + scripts

--- Assets/Scripts/LocalChecker.cs
+++ Assets/Scripts/LocalChecker.cs
@@ -1,5 +1,7 @@
 using Photon.Pun;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.XR.Hands.Samples.VisualizerSample;
 
 public class LocalChecker : MonoBehaviour
 {
@@ -7,16 +9,22 @@ public class LocalChecker : MonoBehaviour
     [SerializeField] private Camera camera;
     [SerializeField] private bool isVr;
     [SerializeField] private GameObject leftHand, rightHand;
+    [SerializeField] private TrackedPoseDriver _trackedPoseDriver;
+    [SerializeField] private HandVisualizer _handVisualizer;
+    
     void Start()
     {
         if (photonView.IsMine)
         {
             camera.enabled = true;
+            _trackedPoseDriver.enabled = isVr;
+            _handVisualizer.enabled = isVr;
         }
         else
         {
             leftHand.SetActive(isVr);
             rightHand.SetActive(isVr);
+            
         }
     }
 
