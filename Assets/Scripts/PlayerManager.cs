4e6b947942d07b19a1b91aadcf30b48a8418d6ee
Mickaël BERTHELOT <41340047+crread@users.noreply.github.com>
2023-04-12 06:39:54 +0200

    [NETWORK] update player vr
    Updated player vr for added views

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
 using UnityEngine;
@@ -11,10 +10,11 @@ public class PlayerManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("test");
 #if UNITY_STANDALONE_WIN
         PhotonNetwork.Instantiate(playerPrefabs[0].name, new Vector3(0, 0, 0), new Quaternion());
 #else
-            PhotonNetwork.Instantiate(playerPrefabs[1].name, new Vector3(0, 0, 0), new Quaternion());
+        PhotonNetwork.Instantiate(playerPrefabs[1].name, new Vector3(0, 0, 0), new Quaternion());
 #endif
     }
 
