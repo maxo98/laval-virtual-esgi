f0455d085c5695dc49d266108474bb9007a7726b
Mickaël BERTHELOT <41340047+crread@users.noreply.github.com>
2023-04-12 13:44:31 +0200

    update main

--- Assets/Scripts/HandDisable.cs
+++ Assets/Scripts/HandDisable.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
@@ -7,28 +8,19 @@ public class HandDisable : MonoBehaviour
 {
     [SerializeField] private PhotonView pv;
 
-    public bool isLeft;
+    [SerializeField] private GameObject handToAttach;
+
 // Start is called before the first frame update
     void Start()
     {
-        if (pv.IsMine)
+        if (!pv.IsMine)
         {
-            if (isLeft)
-            {
-                transform.Find("LeftHand").gameObject.SetActive(false);
-
-            }
-            else
-            {
-                transform.Find("RightHand").gameObject.SetActive(false);
-
-            }
+            handToAttach.transform.SetParent(transform);
         }
-    }
+        else
+        {
+            handToAttach.SetActive(false);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
     }
 }
